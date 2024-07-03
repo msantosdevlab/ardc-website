0b9f6822fdccdd730ab61dfbb177201f1e532073
msantos-lab <msantosdev.lab@gmail.com>
2024-07-03 11:54:20 +0200

    New option on Education projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,8 @@ import FeatureFlags from './featureflags';
 const Categories = Object.freeze({
   EVENT: { label: 'evento', color: 'dark-blue' },
   WORKSHOP: { label: 'workshop', color: 'dark-blue' },
-  MEDIA: { label: 'media', color: 'dark-blue' }
+  MEDIA: { label: 'media', color: 'dark-blue' },
+  ESCOLAS: { label: 'escolas', color: 'dark-blue' }
 });
 
 const LinkType = Object.freeze({
